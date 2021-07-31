34797dd5f9a0e515d4715beb8ecd61a3e631e527
KDWeerasuriya <dilshankelum@gmail.com>
2021-07-31 13:23:44 +0530

    prescriptions function

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\Prescription;
 
 class HomeController extends Controller
 {
@@ -34,4 +35,32 @@ class HomeController extends Controller
     public function pharmacy(){
         return view ("pharmacy");
     }
+    
+     public function prescription(){
+        return view ("prescription");
+    }
+
+    //postprescription update function
+    public function postprescription(Request $request){
+        $prescription = new Prescription;
+        $image = $request->image;
+
+        //define code
+        $imagename = time().'.'.$image->getClientOriginalExtension();
+        $request->image->move('prescriptionimage',$imagename);
+
+        $prescription->image=$imagename;
+        $prescription->cusname=$request->cusname;
+        $prescription->cusaddress=$request->cusaddress;
+        $prescription->cusnic=$request->cusnic;
+        $prescription->cusnumber=$request->cusnumber;
+        $prescription->commen=$request->comment;
+        $prescription->save();
+        return view("prescription");
+    }
+
+
+
+
+
 }
